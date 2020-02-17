541c069ee6c00d570881df562f5b59f5a4fae807
kentonia <60653226+kentonia@users.noreply.github.com>
2020-02-17 16:35:27 +0900

    自動更新機能(修正)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,59 +1,21 @@
-$(function(){ 
-  var buildHTML = function(message) {
-    if (message.content && message.image) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-    } else if (message.content) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-        `</div>` +
-      `</div>`
-    } else if (message.image) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-    };
+$(function(){
+  function buildHTML(message){
+    var img = message.image ? `<img src= ${message.image}>` : "";
+    var html = `<div class="message" data-message-id="${message.id}">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">${message.user_name}</div>
+                    <div class="message__upper-info__date">${message.created_at}</div>
+                  </div>
+                  <div class="message__text">
+                    <p class="lower-message__content">${message.content}</p>
+                    ${img}
+                  </div>
+                </div>`
     return html;
-  };
+  }
   
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(1)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -80,7 +42,6 @@ $(function(){
 
   var reloadMessages = function() {
     last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
     url: "api/messages",
     type: 'get',
@@ -98,7 +59,7 @@ $(function(){
     }
   })
   .fail(function() {
-    console.log('error');
+    alert('自動更新に失敗しました');
     });
   };
   setInterval(reloadMessages, 7000);
